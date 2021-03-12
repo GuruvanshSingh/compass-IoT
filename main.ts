1f38dcddce1724fb23eef9f1157b3eff8d57062e
GuruvanshSingh <79139629+GuruvanshSingh@users.noreply.github.com>
2021-03-11 19:33:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ let Degrees = 0
 basic.forever(function () {
     Degrees = input.compassHeading()
     if (Degrees > 340 || Degrees < 20) {
-        music.playTone(277, music.beat(BeatFraction.Whole))
         basic.showString("N")
         basic.showLeds(`
             . . # . .
@@ -11,6 +10,7 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
+        music.playTone(277, music.beat(BeatFraction.Whole))
     } else if (Degrees > 70 && Degrees < 110) {
         basic.showString("E")
     } else if (Degrees > 160 && Degrees < 200) {
