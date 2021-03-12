932a18eaefb24be32253556993c09e4b33997018
GuruvanshSingh <79139629+GuruvanshSingh@users.noreply.github.com>
2021-03-11 19:14:52 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let Degrees = 0
 basic.forever(function () {
-	
+    Degrees = input.compassHeading()
+    if (Degrees > 340 || Degrees < 20) {
+        music.playTone(277, music.beat(BeatFraction.Whole))
+        basic.showString("N")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
